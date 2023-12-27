9302fb74b9cacc531bc24ac7ae2a436aeea27ee7
sorrel012 <wain1719@gmail.com>
2023-12-27 16:53:37 +0900

    feat: Explore React Elements

--- react-first/react-app/src/index.js
+++ react-first/react-app/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Blog from './chapter_03/Blog';
+import Clock from './chapter_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Blog />
-  </React.StrictMode>,
-);
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>,
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
