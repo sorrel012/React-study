5bc538a6147635a5634326be7915ea4fb7b1eb23
sorrel012 <wain1719@gmail.com>
2024-01-02 13:23:33 +0900

    feat: Explore state

--- react-first/react-app/src/index.js
+++ react-first/react-app/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to sta rt measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
