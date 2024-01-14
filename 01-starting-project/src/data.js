12c1cc0d0b0318307d89ab8d4d5c43e6ae48d9cc
sorrel012 <wain1719@gmail.com>
2024-01-14 23:39:33 +0900

    refactor: Reorder directory

--- 01-starting-project/src/data.js
+++ 01-starting-project/src/data.js
@@ -1,7 +1,7 @@
-import componentsImg from './assets/components.png';
-import propsImg from './assets/config.png';
-import jsxImg from './assets/jsx-ui.png';
-import stateImg from './assets/state-mgmt.png';
+import componentsImg from './assets/image/components.png';
+import propsImg from './assets/image/config.png';
+import jsxImg from './assets/image/jsx-ui.png';
+import stateImg from './assets/image/state-mgmt.png';
 
 export const CORE_CONCEPTS = [
   {
