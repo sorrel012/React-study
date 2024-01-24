c53092e4f827975746ff1e4545849aa50398b47d
sorrel012 <wain1719@gmail.com>
2024-01-24 16:45:46 +0900

    feat: Check player's symbol on the game board

--- 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const initialGameBoard = [
   [null, null, null],
@@ -6,14 +6,28 @@ const initialGameBoard = [
   [null, null, null],
 ];
 function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
