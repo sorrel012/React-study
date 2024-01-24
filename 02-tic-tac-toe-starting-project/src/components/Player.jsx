78734d56d1b3b26f8e333d00cea1732655f5a3b6
sorrel012 <wain1719@gmail.com>
2024-01-24 11:10:18 +0900

    feat: Update state based on old state

--- 02-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,7 +4,7 @@ function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
