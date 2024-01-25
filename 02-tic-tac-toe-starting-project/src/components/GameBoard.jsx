75dd2e3b0e615085c2b5584babeaef4842814fcf
sorrel012 <wain1719@gmail.com>
2024-01-25 11:02:22 +0900

    refactor: Lift state in the GameBoard up to use board information on the Log component

--- 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -5,7 +5,16 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null],
 ];
-function GameBoard({ onSelectSquare }) {
+function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
   //
   // function handleSelectSquare(rowIndex, colIndex) {
@@ -27,7 +36,9 @@ function GameBoard({ onSelectSquare }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
