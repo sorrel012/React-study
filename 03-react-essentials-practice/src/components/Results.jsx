87f3d42cbc01061bcfb389fb25ccae24c3354231
sorrel012 <wain1719@gmail.com>
2024-01-26 15:37:26 +0900

    feat: Show investment calculation result

--- 03-react-essentials-practice/src/components/Results.jsx
+++ 03-react-essentials-practice/src/components/Results.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
-import { calculateInvestmentResults } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 function Results({ input }) {
   const resultData = calculateInvestmentResults(input);
-  return <p>{}</p>;
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map(
+          ({ year, interest, valueEndOfYear, annualInvestment }) => {
+            const totalInterest =
+              valueEndOfYear - annualInvestment * year - initialInvestment;
+            const totalAmountInvested = valueEndOfYear - totalInterest;
+
+            return (
+              <tr key={year}>
+                <td>{year}</td>
+                <td>{formatter.format(valueEndOfYear)}</td>
+                <td>{formatter.format(interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          },
+        )}
+      </tbody>
+    </table>
+  );
 }
 
 export default Results;
