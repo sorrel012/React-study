1eeaf687e4fc6ed4714afa34c4d503f1056917de
sorrel012 <wain1719@gmail.com>
2024-01-29 00:15:27 +0900

    fix: Add $ prefix to props in styled-components to prevent conflicts with built-in attributes

--- 04-styling-react-project/src/components/AuthInputs.jsx
+++ 04-styling-react-project/src/components/AuthInputs.jsx
@@ -22,9 +22,9 @@ const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${({ invalid }) => (invalid ? '#fed2d2' : '#d1d5db')};
-  color: ${({ invalid }) => (invalid ? '#ef4444' : '#374151')};
-  border: 1px solid ${({ invalid }) => (invalid ? '#f73f3f' : 'transparent')};
+  background-color: ${({ $invalid }) => ($invalid ? '#fed2d2' : '#d1d5db')};
+  color: ${({ $invalid }) => ($invalid ? '#ef4444' : '#374151')};
+  border: 1px solid ${({ $invalid }) => ($invalid ? '#f73f3f' : 'transparent')};
   border-radius: 0.25rem;
   box-shadow:
     0 1px 3px 0 rgba(0, 0, 0, 0.1),
