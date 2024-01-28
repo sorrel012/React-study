dc11d2b8def6567b26e2d30c53569871766cda85
sorrel012 <wain1719@gmail.com>
2024-01-29 00:22:44 +0900

    feat: Use pseudo selector: to & prefix

--- 04-styling-react-project/src/components/AuthInputs.jsx
+++ 04-styling-react-project/src/components/AuthInputs.jsx
@@ -31,6 +31,19 @@ const Input = styled.input`
     0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+  
+  &:hover {
+    background-color: #f0920e;
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -77,9 +90,9 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button className="button" onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
