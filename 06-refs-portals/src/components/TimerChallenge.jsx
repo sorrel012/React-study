a6d7122c0e2c3411f6640631a461086344431820
sorrel012 <wain1719@gmail.com>
2024-01-30 22:19:49 +0900

    feat: Set timer and clear timer using Ref

--- 06-refs-portals/src/components/TimerChallenge.jsx
+++ 06-refs-portals/src/components/TimerChallenge.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+      </p>
     </section>
   );
 }
