d19e047b9edbd31cb4b585fa70ab8318483845e9
sorrel012 <wain1719@gmail.com>
2024-02-01 14:50:21 +0900

    feat: Use State and Ref to get users' input

--- 07-advanced-react-practice/src/components/Input.jsx
+++ 07-advanced-react-practice/src/components/Input.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-function Input({ label, textarea, ...props }) {
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
   const classes =
     'w-full p-1 border-b-2 rounded-md border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
 
   return (
-    <p className="flex flex-col gap-1 my-4">
+    <p className="my-4 flex flex-col gap-1">
       <label className="text-sm font-bold uppercase text-stone-600">
         {label}
       </label>
       {textarea ? (
-        <textarea className={`${classes} resize-none`} {...props} />
+        <textarea ref={ref} className={`${classes} resize-none`} {...props} />
       ) : (
-        <input className={classes} {...props} />
+        <input ref={ref} className={classes} {...props} />
       )}
     </p>
   );
-}
+});
 
 export default Input;
--- 07-advanced-react-practice/src/components/NewProject.jsx
+++ 07-advanced-react-practice/src/components/NewProject.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Input from './Input.jsx';
 
 function NewProject() {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+  }
+
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
+    <div className="mt-16 w-[35rem]">
+      <menu className="my-4 flex items-center justify-end gap-4">
         <li>
           <button className="text-stone-800 hover:text-stone-950">
             Cancel
           </button>
         </li>
         <li>
-          <button className=" px-6 py-2 rounded-lg bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className=" rounded-lg bg-stone-800 px-6 py-2 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input ref={dueDate} label="Due Date" />
       </div>
     </div>
   );
