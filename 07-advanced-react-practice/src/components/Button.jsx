62b9976161e9fa90290df84206198bf20f92089b
sorrel012 <wain1719@gmail.com>
2024-02-01 14:32:08 +0900

    refactor: Extract Button component to apply same style

--- 07-advanced-react-practice/src/components/Button.jsx
+++ 07-advanced-react-practice/src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-function Button({ children }) {
+function Button({ children, ...props }) {
   return (
-    <button className="rounded-md bg-stone-700 px-4 py-2 text-xs text-stone-400 hover:bg-stone-600 hover:text-stone-100 md:text-base">
+    <button
+      className="rounded-md bg-stone-700 px-4 py-2 text-xs text-stone-400 hover:bg-stone-600 hover:text-stone-100 md:text-base"
+      {...props}
+    >
       {children}
     </button>
   );
