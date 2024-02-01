5599d2e61bdcb803c259aed5db681882c77738ba
sorrel012 <wain1719@gmail.com>
2024-02-01 15:24:33 +0900

    feat: Create Modal

---
+++ 07-advanced-react-practice/src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+
+function Modal({ children }) {
+  return createPortal(
+    <dialog>{children}</dialog>,
+    document.getElementById('modal-root'),
+  );
+}
+
+export default Modal;
