a926861d4033d518d89a26d7a910dcf183e1c68c
sorrel012 <wain1719@gmail.com>
2024-02-01 14:10:51 +0900

    refactor: Extract image path and order tailwind-css

--- 07-advanced-react-practice/src/components/NoProjectSeleted.jsx
+++ 07-advanced-react-practice/src/components/NoProjectSeleted.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import noProjectImage from '../assets/no-projects.png';
 
 function NoProjectSeleted() {
   return (
-    <div>
-      <img src="../assets/no-projects.png" alt="Image that has no project" />
-      <h2>No Project Selected</h2>
+    <div className="mt-24 w-2/3 text-center">
+      <img
+        src={noProjectImage}
+        alt="An empty task list"
+        className="h-16 w-16"
+      />
+      <h2 className="my-4 text-xl font-bold text-stone-500">
+        No Project Selected
+      </h2>
       <p>Select a project or get started with a new one</p>
       <p>
         <button>Create new project</button>
