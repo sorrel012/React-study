144520ed327436a8bf6dc95137b7c6e9d415fb55
sorrel012 <wain1719@gmail.com>
2024-02-02 10:05:08 +0900

    refactor: Send function using Context API instead of props drilling

--- 08-advanced-state-management/src/components/Product.jsx
+++ 08-advanced-state-management/src/components/Product.jsx
@@ -14,7 +14,7 @@ export default function Product({ id, image, title, price, description }) {
           <p>{description}</p>
         </div>
         <p className="product-actions">
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+          <button onClick={() => addItemToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
