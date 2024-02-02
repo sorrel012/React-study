f3ed15e42fb3ed94aea31f8eab4ec374ba27ccfc
sorrel012 <wain1719@gmail.com>
2024-02-02 16:52:50 +0900

    feat: Add style to quiz component

--- 10-effects-practice/src/components/Quiz.jsx
+++ 10-effects-practice/src/components/Quiz.jsx
@@ -13,15 +13,19 @@ function Quiz() {
   }
 
   return (
-    <div id="question">
-      <p>{QUESTIONS[activeQuestionIndex].text}</p>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id="quiz">
+      <div id="question">
+        <p>{QUESTIONS[activeQuestionIndex].text}</p>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
