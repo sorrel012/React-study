aa7aa42fe29c10b0ea0ad90ea449a96957437a60
sorrel012 <wain1719@gmail.com>
2024-02-04 14:33:12 +0900

    feat: Initialize timer when the answer is selected

--- 10-effects-practice/src/components/QuestionTimer.jsx
+++ 10-effects-practice/src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-function QuestionTimer({ timeout, onTimeout }) {
+function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
@@ -21,7 +21,14 @@ function QuestionTimer({ timeout, onTimeout }) {
     };
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime} />;
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 }
 
 export default QuestionTimer;
