ba0397c9024b2ad2bb24b988dc2cdf002670b443
sorrel012 <wain1719@gmail.com>
2024-02-04 14:44:47 +0900

    feat: Show quiz result percent

--- 10-effects-practice/src/components/Summary.jsx
+++ 10-effects-practice/src/components/Summary.jsx
@@ -3,22 +3,35 @@ import quizCompleteImg from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
 
 function Summary({ userAnswers }) {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, i) => answer === QUESTIONS[i].answers[0],
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100,
+  );
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100,
+  );
+  const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
+
   return (
     <div id="summary">
       <img src={quizCompleteImg} alt="Quiz complete(Trophy icon)" />
       <h2>Quiz Completed!</h2>
       <div id="summary-stats">
         <p>
-          <span className="number"></span>
-          <span className="text"></span>
+          <span className="number">{skippedAnswersShare}%</span>
+          <span className="text">skipped</span>
         </p>
         <p>
-          <span className="number"></span>
-          <span className="text"></span>
+          <span className="number">{correctAnswersShare}%</span>
+          <span className="text">answered correctly</span>
         </p>
         <p>
-          <span className="number"></span>
-          <span className="text"></span>
+          <span className="number">{wrongAnswersShare}%</span>
+          <span className="text">answered incorrectly</span>
         </p>
       </div>
       <ol>
@@ -34,7 +47,7 @@ function Summary({ userAnswers }) {
           }
 
           return (
-            <li key={answer}>
+            <li key={i}>
               <h3>{i + 1}</h3>
               <p className="question">{QUESTIONS[i].text}</p>
               <p className={cssClass}>{answer ?? 'Skipped'}</p>
