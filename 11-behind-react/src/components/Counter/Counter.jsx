f72d0a2b5883725a401f84e5bacb5d2d3fa77baa
sorrel012 <wain1719@gmail.com>
2024-02-05 17:08:58 +0900

    refactor: Extract ConfigureCounter component to improve performance by preventing re-rendering

--- 11-behind-react/src/components/Counter/Counter.jsx
+++ 11-behind-react/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react';
+import { useState } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -23,7 +23,7 @@ function isPrime(number) {
   return true;
 }
 
-const Counter = memo(function Counter({ initialCount }) {
+function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
   const initialCountIsPrime = isPrime(initialCount);
 
@@ -54,6 +54,6 @@ const Counter = memo(function Counter({ initialCount }) {
       </p>
     </section>
   );
-});
+}
 
 export default Counter;
