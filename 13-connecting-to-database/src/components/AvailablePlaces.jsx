671857bad8d18b8f0234367880f8ae047a55c83d
sorrel012 <wain1719@gmail.com>
2024-02-06 15:48:54 +0900

    feat: Add loading states

--- 13-connecting-to-database/src/components/AvailablePlaces.jsx
+++ 13-connecting-to-database/src/components/AvailablePlaces.jsx
@@ -2,20 +2,27 @@ import Places from './Places.jsx';
 import { useEffect, useState } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places');
-      const resData = response.json();
+      const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
