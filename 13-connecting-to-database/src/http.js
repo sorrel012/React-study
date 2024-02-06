96885372ea799b336bf31b4069a6ccce34ea010b
sorrel012 <wain1719@gmail.com>
2024-02-06 17:44:50 +0900

    feat: Get user places when the app started

--- 13-connecting-to-database/src/http.js
+++ 13-connecting-to-database/src/http.js
@@ -10,6 +10,16 @@ export async function fetchAvailableFunction() {
   return resData.places;
 }
 
+export async function fetchUserPlaces() {
+  const response = await fetch('http://localhost:3000/user-places');
+  if (!response.ok) {
+    throw new Error('Failed to fetch user places');
+  }
+  const resData = await response.json();
+
+  return resData.places;
+}
+
 export async function updateUserPlaces(places) {
   const response = await fetch('http://localhost:3000/user-places', {
     method: 'PUT',
