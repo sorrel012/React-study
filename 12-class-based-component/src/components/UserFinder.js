da1d4eb4b65c32dd10074f2d03c7a07273515b52
sorrel012 <wain1719@gmail.com>
2024-02-06 13:37:00 +0900

    feat: Explore React error boundary

--- 12-class-based-component/src/components/UserFinder.js
+++ 12-class-based-component/src/components/UserFinder.js
@@ -3,6 +3,7 @@ import { Component, Fragment } from 'react';
 import Users from './Users';
 import classes from './UserFinder.module.css';
 import UsersContext from '../store/users-context';
+import ErrorBoundary from './ErrorBoundary';
 
 class UserFinder extends Component {
   static contextType = UsersContext;
@@ -42,7 +43,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
