24ee74ca3dbedc5dcd8764ee8a48deb7d4f32c04
sorrel012 <wain1719@gmail.com>
2024-02-06 17:11:16 +0900

    feat: Send places data using fetch(put) to server

--- 13-connecting-to-database/src/http.js
+++ 13-connecting-to-database/src/http.js
@@ -9,3 +9,23 @@ export async function fetchAvailableFunction() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({
+      places: places,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to update user places.');
+  }
+
+  const resData = await response.json();
+
+  return resData.message;
+}
