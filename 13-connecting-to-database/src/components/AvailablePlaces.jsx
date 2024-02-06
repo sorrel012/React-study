d3080407b6e2fd89ba268471b730faa77fe50167
sorrel012 <wain1719@gmail.com>
2024-02-06 16:18:02 +0900

    feat: Handle fetching error

--- 13-connecting-to-database/src/components/AvailablePlaces.jsx
+++ 13-connecting-to-database/src/components/AvailablePlaces.jsx
@@ -1,21 +1,38 @@
 import Places from './Places.jsx';
 import { useEffect, useState } from 'react';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
+  async function fetchPlaces() {
+    setIsFetching(true);
+    try {
+      const response = await fetch('http://localhost:3000/place');
+      if (!response.ok) {
+        throw new Error('Failed to fetch places');
+      }
       const resData = await response.json();
+
       setAvailablePlaces(resData.places);
-      setIsFetching(false);
+    } catch (error) {
+      setError({
+        message:
+          error.message || 'Could not fetch places, please try again later',
+      });
     }
 
+    setIsFetching(false);
+  }
+  useEffect(() => {
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />;
+  }
 
   return (
     <Places
