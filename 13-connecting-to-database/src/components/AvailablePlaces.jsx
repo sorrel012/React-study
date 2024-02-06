475dce29e16d7fded0e7490a0b2aeeb94f7b2519
sorrel012 <wain1719@gmail.com>
2024-02-06 14:46:13 +0900

    refactor: Use await keyword

--- 13-connecting-to-database/src/components/AvailablePlaces.jsx
+++ 13-connecting-to-database/src/components/AvailablePlaces.jsx
@@ -5,13 +5,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = response.json();
+      setAvailablePlaces(resData.places);
+    }
   }, []);
 
   return (
