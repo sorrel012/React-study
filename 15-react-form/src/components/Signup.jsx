3ff28a8748db59cd04042ece6a1721db652e8aa3
sorrel012 <wain1719@gmail.com>
2024-02-07 17:02:30 +0900

    feat: Explore FormData

--- 15-react-form/src/components/Signup.jsx
+++ 15-react-form/src/components/Signup.jsx
@@ -1,6 +1,14 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = fd.getAll('acquisition');
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
