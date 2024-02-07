77d99370851bc94041326e4cc43c8ae4cfb83229
sorrel012 <wain1719@gmail.com>
2024-02-07 17:21:33 +0900

    refactor: Check the email validation with ref

--- 15-react-form/src/components/Login.jsx
+++ 15-react-form/src/components/Login.jsx
@@ -1,12 +1,25 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(email.current.value);
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
 
   return (
@@ -17,6 +30,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
