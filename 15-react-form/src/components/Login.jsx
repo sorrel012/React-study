d9eb1673c6aa4953225f5db26cd58aca48c6c2f3
sorrel012 <wain1719@gmail.com>
2024-02-07 15:59:15 +0900

    feat: Add onSubmit when the Login button is clicked

--- 15-react-form/src/components/Login.jsx
+++ 15-react-form/src/components/Login.jsx
@@ -1,6 +1,10 @@
 export default function Login() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
