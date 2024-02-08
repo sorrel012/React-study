56c65bbdb744d5ad75093de8c338ada4fdf1dcac
sorrel012 <wain1719@gmail.com>
2024-02-08 11:01:39 +0900

    feat: Display meals

--- 16-http-reqeust-practice/src/components/Meals.jsx
+++ 16-http-reqeust-practice/src/components/Meals.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 function Meals({ meals }) {
   return (
-    <div>
+    <div id="meals">
       {meals.map((meal) => {
         return (
-          <div key={meal.id}>
-            <img src={meal.image} alt="Meal img" />
-            <div>{meal.name}</div>
-            <div>{meal.price}</div>
-            <div>{meal.description}</div>
-            <button>Add to Cart</button>
+          <div key={meal.id} className="meal-item">
+            <div>
+              <article>
+                <img
+                  src={`http://localhost:3000/${meal.image}`}
+                  alt="Meal img"
+                />
+                <h3>{meal.name}</h3>
+                <div className="meal-item-price">{meal.price}</div>
+                <div className="meal-item-description">{meal.description}</div>
+              </article>
+              <button className="button meal-item-actions">Add to Cart</button>
+            </div>
           </div>
         );
       })}
