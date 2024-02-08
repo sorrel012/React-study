6582ad96ccfd5aeae8d74c20687b572169295a9c
sorrel012 <wain1719@gmail.com>
2024-02-08 17:51:03 +0900

    feat: Add item to cart using reducer

---
+++ 16-http-reqeust-practice/src/store/CarContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useReducer } from 'react';
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === 'ADD_ITEM') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id,
+    );
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [cartState, cartDispatch] = useReducer(cartReducer, { items: [] });
+
+  return (
+    <CartContext.Provider value={cartState}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartContext;
