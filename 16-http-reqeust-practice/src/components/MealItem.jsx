76a35a3b7a81f984d33aef229128c7133b0d1fbd
sorrel012 <wain1719@gmail.com>
2024-02-08 18:04:25 +0900

    feat: Use addItem function using context

--- 16-http-reqeust-practice/src/components/MealItem.jsx
+++ 16-http-reqeust-practice/src/components/MealItem.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { currencyFormatter } from '../util/formatting.js';
 import Button from './UI/Button.jsx';
+import CartContext from '../store/CarContext.jsx';
 
 function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -15,7 +22,7 @@ function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button textOnly={false} onClick={() => handleAddCart(meal)}>
+          <Button textOnly={false} onClick={handleAddMealToCart}>
             Add to Cart
           </Button>
         </p>
