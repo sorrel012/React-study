1aaf0b0376b43be451e5c5cda226a2b242c8b1fe
sorrel012 <wain1719@gmail.com>
2024-02-09 16:13:48 +0900

    fix: Revise className of input

--- 16-http-reqeust-practice/src/components/UI/Input.jsx
+++ 16-http-reqeust-practice/src/components/UI/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Input({ label, id, ...props }) {
   return (
-    <p className="controll">
+    <p className="control">
       <label htmlFor={id}>{label}</label>
       <input id={id} name={id} {...props} required />
     </p>
