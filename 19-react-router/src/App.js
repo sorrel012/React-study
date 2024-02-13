dd5e7576135adbcdcfda3ba50579fa9cdcfe313b
sorrel012 <wain1719@gmail.com>
2024-02-13 23:21:47 +0900

    feat: Define route

--- 19-react-router/src/App.js
+++ 19-react-router/src/App.js
@@ -1,3 +1,7 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
 function App() {
   return <div></div>;
 }
