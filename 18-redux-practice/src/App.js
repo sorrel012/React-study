6c99b60bec4ed3e0e210fb3e8ff3da3133ed6c5b
sorrel012 <wain1719@gmail.com>
2024-02-13 16:47:46 +0900

    feat: Store the cart data to Firebase

--- 18-redux-practice/src/App.js
+++ 18-redux-practice/src/App.js
@@ -2,9 +2,19 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    axios.put(
+      'https://react-http-dd472-default-rtdb.firebaseio.com/cart.json',
+      cart,
+    );
+  }, [cart]);
 
   return (
     <Layout>
