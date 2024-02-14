73855e317d4409323aef22b74518ab5a58ca48ed
sorrel012 <wain1719@gmail.com>
2024-02-15 00:12:52 +0900

    feat: Output a list of dummy events to the EventsPage

--- 20-router-practice/frontend/src/App.js
+++ 20-router-practice/frontend/src/App.js
@@ -1,6 +1,5 @@
 // Challenge / Exercise
 
-// 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage.js
 // 7. Output the ID of the selected event on the EventDetailPage.js
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
