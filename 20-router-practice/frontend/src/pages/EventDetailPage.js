c1cccc0007bc0cf2033402bce7284be7bd792eec
sorrel012 <wain1719@gmail.com>
2024-02-15 00:33:41 +0900

    feat: Display EventItem

--- 20-router-practice/frontend/src/pages/EventDetailPage.js
+++ 20-router-practice/frontend/src/pages/EventDetailPage.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
-  return <h1>Event Detail Page</h1>;
+  const params = useParams();
+  const eventId = params.eventId;
+  const events = useSelector((state) => state.events);
+  const event = events.filter((event) => event.id === eventId);
+
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <EventItem event={event} />
+    </>
+  );
 }
 
 export default EventDetailPage;
--- 20-router-practice/frontend/src/pages/EventsPage.js
+++ 20-router-practice/frontend/src/pages/EventsPage.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { getEvents } from '../plugins/eventAxios';
+import React, { useCallback, useEffect, useState } from 'react';
 import EventsList from '../components/EventsList';
 import { useDispatch } from 'react-redux';
+import { getEvents } from '../plugins/eventAxios';
 import { eventActions } from '../store/event-slice';
 
 function EventsPage() {
   const dispatch = useDispatch();
   const [events, setEvents] = useState([]);
 
-  const getEventsData = async () => {
+  const getEventsData = useCallback(async () => {
     const data = await getEvents();
     const eventData = data.result;
     setEvents(eventData);
-    dispatch(eventActions.replaceEvent, eventData);
-  };
+    dispatch(eventActions.replaceEvent(eventData));
+  }, [dispatch]);
 
   useEffect(() => {
     getEventsData();
