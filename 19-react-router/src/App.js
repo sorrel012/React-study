c55d28b51f34db40d9f017694d3ae222e8aeac1c
sorrel012 <wain1719@gmail.com>
2024-02-14 13:56:06 +0900

    feat: Use children property and wrap components

--- 19-react-router/src/App.js
+++ 19-react-router/src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -9,8 +10,14 @@ import ProductsPage from './pages/Products';
 // );
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
