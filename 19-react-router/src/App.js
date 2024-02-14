c54c3cf899c5be28d13a5f735f3fc7450d65a59c
sorrel012 <wain1719@gmail.com>
2024-02-14 16:31:29 +0900

    feat: Implement dynamic route

--- 19-react-router/src/App.js
+++ 19-react-router/src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> },
     ],
   },
 ]);
--- 19-react-router/src/pages/Products.js
+++ 19-react-router/src/pages/Products.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ProductsPage() {
-  return <h1>The Products Page</h1>;
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/product-1">Product 1</Link>
+        </li>
+        <li>
+          <Link to="/products/product-2">Product 2</Link>
+        </li>
+        <li>
+          <Link to="/products/product-3">Product 3</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default ProductsPage;
