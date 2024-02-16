8597902367148a526aae8fd597cf336edaf8e575
sorrel012 <wain1719@gmail.com>
2024-02-16 22:24:13 +0900

    fix: Use relative path

--- 20-router-practice/frontend/src/pages/EventDetail.js
+++ 20-router-practice/frontend/src/pages/EventDetail.js
@@ -1,18 +1,38 @@
-import { getEventDetail } from '../plugins/eventAxios';
-import { json, redirect, useRouteLoaderData } from 'react-router-dom';
-import EventItem from '../components/EventItem';
+import { getEventDetail, getEvents } from '../plugins/eventAxios';
+import {
+  Await,
+  defer,
+  json,
+  redirect,
+  useRouteLoaderData,
+} from 'react-router-dom';
 import axios from 'axios';
+import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventDetailPage() {
-  const event = useRouteLoaderData('event-detail');
+  const { event, events } = useRouteLoaderData('event-detail');
 
-  return <>{<EventItem event={event} />}</>;
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadEvents) => <EventsList events={loadEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  const id = params.eventId;
+async function loadEvent(id) {
   const data = await getEventDetail(id);
 
   if (data.status === 'FAIL') {
@@ -25,6 +45,30 @@ export async function loader({ request, params }) {
   } else if (data.status === 'SUCCESS') {
     return data.result.event;
   }
+}
+
+async function loadEvents() {
+  const data = await getEvents();
+
+  if (data.status === 'FAIL') {
+    throw json(
+      { message: 'Could not fetch Events' },
+      {
+        status: 500,
+      },
+    );
+  } else if (data.status === 'SUCCESS') {
+    return data.result;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  return defer({
+    event: loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ params, request }) {
