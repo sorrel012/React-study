5f34cb077fb4b0d249311536e8d30b1e77ef0174
sorrel012 <wain1719@gmail.com>
2024-02-16 14:23:15 +0900

    feat: Use useNavigation to get action state

--- 20-router-practice/frontend/src/components/EventForm.js
+++ 20-router-practice/frontend/src/components/EventForm.js
@@ -1,9 +1,13 @@
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  const navigation = useNavigation();
   const navigate = useNavigate();
+
+  const isSubmitting = navigation.state === 'submitting';
+
   function cancelHandler() {
     navigate('..');
   }
@@ -54,7 +58,9 @@ function EventForm({ method, event }) {
         <button type="button" onClick={cancelHandler}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' : 'Save'}
+        </button>
       </div>
     </Form>
   );
