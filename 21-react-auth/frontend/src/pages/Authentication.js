1a5b3dc11bf9af5e8af99bdc61e0ade9f71cc5bd
sorrel012 <wain1719@gmail.com>
2024-02-17 19:18:37 +0900

    feat: Send user data and get errors

--- 21-react-auth/frontend/src/pages/Authentication.js
+++ 21-react-auth/frontend/src/pages/Authentication.js
@@ -8,12 +8,39 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
+// export async function action({ request }) {
+//   const searchParams = new URL(request.url).searchParams;
+//   const mode = searchParams.get('mode') || 'login';
+//
+//   if (mode !== 'login' && mode !== 'signup') {
+//     throw json({ message: 'Unsupported mode.' }, { status: 422 });
+//   }
+//
+//   const data = await request.formData();
+//   const authData = {
+//     email: data.get('email'),
+//     password: data.get('password'),
+//   };
+//
+//   const result = await requestAuth(authData, mode);
+//
+//   if (result.result.status === 422 || result.result.status === 401) {
+//     return result.result.data;
+//   }
+//
+//   if (result.status === 'FAIL') {
+//     throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+//   }
+//
+//   return redirect('/');
+// }
+
 export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get('mode') || 'login';
 
   if (mode !== 'login' && mode !== 'signup') {
-    throw json({ message: 'Unsupported mode.' }, { status: 422 });
+    throw new Error('Unsupported mode.', { status: 422 });
   }
 
   const data = await request.formData();
@@ -22,19 +49,20 @@ export async function action({ request }) {
     password: data.get('password'),
   };
 
-  await axios
-    .post('http://localhost:8080/' + mode, authData)
-    .then((response) => {})
-    .catch((error) => {
-      if (error.status === 422 || error.status === 401) {
-        return error;
-      }
+  try {
+    const response = await axios.post(
+      `http://localhost:8080/${mode}`,
+      authData,
+    );
 
-      throw json(
-        { message: 'Could not authenticate user. mode.' },
-        { status: 500 },
-      );
-    });
-
-  return redirect('/');
+    if (response.statusText === 'OK' || response.statusText === 'Created') {
+      return redirect('/');
+    }
+  } catch (error) {
+    if (error.response.status === 422 || error.response.status === 401) {
+      return error.response.data;
+    } else {
+      throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+    }
+  }
 }
