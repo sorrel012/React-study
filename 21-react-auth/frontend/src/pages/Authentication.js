43ac2c960581baa04355e6c83b7aa890f50f5fc1
sorrel012 <wain1719@gmail.com>
2024-02-17 18:09:38 +0900

    chore: Implement Login or SignUp

--- 21-react-auth/frontend/src/pages/Authentication.js
+++ 21-react-auth/frontend/src/pages/Authentication.js
@@ -1,7 +1,40 @@
 import AuthForm from '../components/AuthForm';
+import { json, redirect } from 'react-router-dom';
+import axios from 'axios';
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  await axios
+    .post('http://localhost:8080/' + mode, authData)
+    .then((response) => {})
+    .catch((error) => {
+      if (error.status === 422 || error.status === 401) {
+        return error;
+      }
+
+      throw json(
+        { message: 'Could not authenticate user. mode.' },
+        { status: 500 },
+      );
+    });
+
+  return redirect('/');
+}
