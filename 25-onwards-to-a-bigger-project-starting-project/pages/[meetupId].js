f354d9b584699120015684552898fc94dff97573
sorrel012 <wain1719@gmail.com>
2024-02-20 16:50:08 +0900

    feat: Split meetup detail component from [meetupId]

---
+++ 25-onwards-to-a-bigger-project-starting-project/pages/[meetupId].js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Lehesten_2012_x14.JPG/1024px-Lehesten_2012_x14.JPG"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
