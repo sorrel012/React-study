f826a328aa9dd8c32514b44fa5e63497688269fe
sorrel012 <wain1719@gmail.com>
2024-02-20 16:26:40 +0900

    feat: Design layout

--- 25-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList';
+import Layout from '../components/layout/Layout';
 
 const DUMMY_MEETUPS = [
   {
@@ -12,7 +13,11 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
 }
 
 export default HomePage;
