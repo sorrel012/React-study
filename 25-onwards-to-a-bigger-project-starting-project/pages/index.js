c8b8550f551ddbe05963a744b1b0e30246f4f563
sorrel012 <wain1719@gmail.com>
2024-02-20 16:14:44 +0900

    feat: Add the main page

--- 25-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,5 +1,18 @@
-function MyComponent() {
-  return <></>;
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Lehesten_2012_x14.JPG/1024px-Lehesten_2012_x14.JPG',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
-export default MyComponent;
+export default HomePage;
