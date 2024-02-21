4859fdbc6b0432966eb969a5cd5c56b90ef0832c
sorrel012 <wain1719@gmail.com>
2024-02-21 15:38:34 +0900

    refactor: Extract Pool object

--- 25-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -1,12 +1,4 @@
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-});
+import { pool } from '../../config/connectDB';
 
 async function handler(req, res) {
   console.log(req, 'req');
