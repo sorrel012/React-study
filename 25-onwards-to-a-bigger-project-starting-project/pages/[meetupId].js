c441e53d2224d3396a7f128056eb79365cef2fd3
sorrel012 <wain1719@gmail.com>
2024-02-21 10:50:58 +0900

    feat: Use getStaticPaths to set dynamic routing

--- 25-onwards-to-a-bigger-project-starting-project/pages/[meetupId].js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/[meetupId].js
@@ -1,17 +1,43 @@
 import MeetupDetail from '../components/meetups/MeetupDetail';
 
 function MeetupDetails(props) {
+  const data = props.meetupData;
 
-  return <MeetupDetail props.meetupData/>;
+  return (
+    <MeetupDetail
+      image={data.image}
+      title={data.title}
+      address={data.address}
+      description={data.description}
+    />
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
 }
 
 export function getStaticProps(context) {
-
   const meetupId = context.params.meetupId;
 
   return {
     props: {
       meetupData: {
+        id: meetupId,
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Lehesten_2012_x14.JPG/1024px-Lehesten_2012_x14.JPG',
         title: 'A First Meetup',
