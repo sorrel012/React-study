b35444f692f636909a52d032062008308d4f46ec
sorrel012 <wain1719@gmail.com>
2024-02-21 14:38:34 +0900

    feat: Get data from postgreSQL

--- 25-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -1,6 +1,36 @@
-function handler(req, res) {
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+});
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { title, image, address, description } = req.body;
+
+    try {
+      const client = await pool.connect();
+
+      const result = await client.query(
+        'INSERT INTO meetup(title, image, address, description) VALUES($1, $2, $3, $4) RETURNING *',
+        [title, image, address, description],
+      );
+
+      client.release();
+
+      // 응답을 클라이언트에 보냄
+      res.status(201).json({ message: 'Meetup inserted!' });
+    } catch (err) {
+      console.error('Database connection error', err.stack);
+      res.status(500).json({ message: 'Database connection failed' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
 
