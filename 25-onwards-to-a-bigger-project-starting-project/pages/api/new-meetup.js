0fa0be581d8cd6e3d30728f57df9196c5a224e8b
sorrel012 <wain1719@gmail.com>
2024-02-21 15:25:57 +0900

    feat: Store data

--- 25-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -9,6 +9,7 @@ const pool = new Pool({
 });
 
 async function handler(req, res) {
+  console.log(req, 'req');
   if (req.method === 'POST') {
     const { title, image, address, description } = req.body;
 
--- 25-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,8 +1,16 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import axios from 'axios';
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  async function addMeetupHandler(enteredMeetupData) {
+    await axios
+      .post('/api/new-meetup', enteredMeetupData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
