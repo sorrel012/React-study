9ba29e601e1600f61d2e4afce5553dc5250f5b64
sorrel012 <wain1719@gmail.com>
2024-02-21 10:33:01 +0900

    feat: Get meetup detail data using getStaticProps

--- 25-onwards-to-a-bigger-project-starting-project/pages/[meetupId].js
+++ 25-onwards-to-a-bigger-project-starting-project/pages/[meetupId].js
@@ -1,14 +1,25 @@
 import MeetupDetail from '../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
-  return (
-    <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Lehesten_2012_x14.JPG/1024px-Lehesten_2012_x14.JPG"
-      title="A First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
-    />
-  );
+function MeetupDetails(props) {
+
+  return <MeetupDetail props.meetupData/>;
+}
+
+export function getStaticProps(context) {
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Lehesten_2012_x14.JPG/1024px-Lehesten_2012_x14.JPG',
+        title: 'A First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
