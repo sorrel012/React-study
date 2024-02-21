0229ff24b20a6733352d2eafcc3b00be4f5e3dca
sorrel012 <wain1719@gmail.com>
2024-02-21 16:24:34 +0900

    feat: Install uuid extension module

--- 25-onwards-to-a-bigger-project-starting-project/script.sql
+++ 25-onwards-to-a-bigger-project-starting-project/script.sql
@@ -1,5 +1,6 @@
 create table MEETUP 
 (
+	id UUID primary key DEFAULT uuid_generate_v4(),
 	title varchar(5000) not null,
 	image varchar(5000) not null,
 	address varchar(1000) not null,
@@ -7,3 +8,8 @@ create table MEETUP
 ) 
 
 select * from MEETUP;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+select id from meetup;
+
