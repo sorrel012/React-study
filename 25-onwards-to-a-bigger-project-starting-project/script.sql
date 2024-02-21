673a4e5058c0d4610eeb4472d6a8177b33afff8e
sorrel012 <wain1719@gmail.com>
2024-02-21 15:25:11 +0900

    fix: Correct spell

--- 25-onwards-to-a-bigger-project-starting-project/script.sql
+++ 25-onwards-to-a-bigger-project-starting-project/script.sql
@@ -1,7 +1,9 @@
 create table MEETUP 
 (
 	title varchar(5000) not null,
-	imgage varchar(5000) not null,
+	image varchar(5000) not null,
 	address varchar(1000) not null,
 	description varchar(5000) not null
 ) 
+
+select * from MEETUP;
