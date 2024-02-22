da3367143fb8ce5a9de5392bf76ded4034991299
sorrel012 <wain1719@gmail.com>
2024-02-22 23:21:40 +0900

    feat: Manipulate many animation's order

--- 26-react-animation/src/components/Challenges.jsx
+++ 26-react-animation/src/components/Challenges.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from 'react';
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import ChallengeItem from './ChallengeItem.jsx';
 import ChallengeTabs from './ChallengeTabs.jsx';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Challenges() {
   const { challenges } = useContext(ChallengesContext);
@@ -26,7 +27,7 @@ export default function Challenges() {
   const filteredChallenges = {
     active: challenges.filter((challenge) => challenge.status === 'active'),
     completed: challenges.filter(
-      (challenge) => challenge.status === 'completed'
+      (challenge) => challenge.status === 'completed',
     ),
     failed: challenges.filter((challenge) => challenge.status === 'failed'),
   };
@@ -40,18 +41,25 @@ export default function Challenges() {
         onSelectType={handleSelectType}
         selectedType={selectedType}
       >
-        {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
-            {displayedChallenges.map((challenge) => (
-              <ChallengeItem
-                key={challenge.id}
-                challenge={challenge}
-                onViewDetails={() => handleViewDetails(challenge.id)}
-                isExpanded={expanded === challenge.id}
-              />
-            ))}
-          </ol>
-        )}
+        <AnimatePresence>
+          {displayedChallenges.length > 0 && (
+            <motion.ol
+              exit={{ y: -30, opacity: 0 }}
+              className="challenge-items"
+            >
+              <AnimatePresence>
+                {displayedChallenges.map((challenge) => (
+                  <ChallengeItem
+                    key={challenge.id}
+                    challenge={challenge}
+                    onViewDetails={() => handleViewDetails(challenge.id)}
+                    isExpanded={expanded === challenge.id}
+                  />
+                ))}
+              </AnimatePresence>
+            </motion.ol>
+          )}
+        </AnimatePresence>
         {displayedChallenges.length === 0 && <p>No challenges found.</p>}
       </ChallengeTabs>
     </div>
