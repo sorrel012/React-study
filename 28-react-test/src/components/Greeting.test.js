f8e77b140caad9d2d82bdfc0ac73c103f16886d2
sorrel012 <wain1719@gmail.com>
2024-02-23 17:37:24 +0900

    feat: Add dynamic test

--- 28-react-test/src/components/Greeting.test.js
+++ 28-react-test/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import Greeting from './Greeting';
+import userEvent from '@testing-library/user-event';
 
 describe('Greeting component', () => {
   test('renders Hello World as a text', () => {
@@ -13,5 +14,34 @@ describe('Greeting component', () => {
       exact: false,
     });
     expect(hellowWorldElement).toBeInTheDocument;
+  });
+
+  test('renders good to see you if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText('good to see you', {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument;
+  });
+
+  test('renders Changed! if the button was clicked', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument;
+  });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).not.toBeInTheDocument;
   });
 });
