48b87f89031cf1baa3e115988c93c4f41a212890
sorrel012 <wain1719@gmail.com>
2024-03-03 17:04:54 +0900

    feat: Style main and App component

--- 29-react-ts/src/App.tsx
+++ 29-react-ts/src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css';
 import Todos from './components/Todos.tsx';
 import NewTodo from './components/NewTodo.tsx';
 import { useSelector } from 'react-redux';
 import Todo from './models/todo.ts';
+import './App.css';
 
 function App() {
   const todos = useSelector((state: { todos: Todo[] }) => state.todos);
