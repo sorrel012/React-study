fde0f62f01630d2b518e509cc9e857cc69f71b49
sorrel012 <wain1719@gmail.com>
2024-03-03 17:05:54 +0900

    rename: Create css directory and move the css & scss files into the directory

--- 29-react-ts/src/App.tsx
+++ 29-react-ts/src/App.tsx
@@ -2,7 +2,7 @@ import Todos from './components/Todos.tsx';
 import NewTodo from './components/NewTodo.tsx';
 import { useSelector } from 'react-redux';
 import Todo from './models/todo.ts';
-import './App.css';
+import './asset/css/App.css';
 
 function App() {
   const todos = useSelector((state: { todos: Todo[] }) => state.todos);
--- 29-react-ts/src/main.tsx
+++ 29-react-ts/src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import './main.css';
+import './asset/css/main.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
