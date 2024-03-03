b1d188d0f4e43a8198da32a6900912885f1f45d4
sorrel012 <wain1719@gmail.com>
2024-03-03 14:38:56 +0900

    feat: Set id as unique code

--- 29-react-ts/src/models/todo.ts
+++ 29-react-ts/src/models/todo.ts
@@ -4,7 +4,7 @@ class Todo {
 
   constructor(todoText: string) {
     this.text = todoText;
-    this.id = new Date().toISOString();
+    this.id = new Date().toISOString() + Math.floor(Math.random() * 100);
   }
 }
 
