2e7845fef571c1f4870f3df715e73140aa00c88d
sorrel012 <wain1719@gmail.com>
2024-03-03 16:23:17 +0900

    refactor: Define types

--- 29-react-ts/src/store/todoSlice.ts
+++ 29-react-ts/src/store/todoSlice.ts
@@ -1,13 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import Todo from '../models/todo.ts';
+
+interface TodoState {
+  todos: Todo[];
+}
+
+const initialState: TodoState = {
+  todos: [],
+};
 
 export const todoSlice = createSlice({
   name: 'todo',
-  initialState: { todos: [] },
+  initialState,
   reducers: {
-    addTodo(state, action) {
+    addTodo(state, action: PayloadAction<Todo>) {
       state.todos.push(action.payload);
     },
-    removeTodo(state, action) {
+    removeTodo(state, action: PayloadAction<string>) {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
   },
