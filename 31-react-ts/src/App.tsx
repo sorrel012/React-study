2a45d3e2b36e7b3bce4f5369e41f8819cddb50bc
sorrel012 <wain1719@gmail.com>
2024-03-05 21:57:42 +0900

    feat: Set theme using styled-components

--- 31-react-ts/src/App.tsx
+++ 31-react-ts/src/App.tsx
@@ -1,31 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState('');
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('hello', value);
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="username"
-          value={value}
-          onChange={handleChange}
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>props</H1>
+    </Container>
   );
 }
 
