3c2bbd7fbed58fa19f468628505f5d0c96de79e7
sorrel012 <wain1719@gmail.com>
2024-03-05 14:48:40 +0900

    feat: Put styles on the divs

--- 30-react-styling/src/App.js
+++ 30-react-styling/src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return;
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ backgroundColor: 'teal', width: 100, height: 100 }}></div>
+      <div style={{ backgroundColor: 'tomato', width: 100, height: 100 }}></div>
+    </div>
+  );
 }
 
 export default App;
