5667f9f98b1c210be9003edddf5e987f8594895d
sorrel012 <wain1719@gmail.com>
2024-03-05 22:50:38 +0900

    feat: Use children props in the router

--- 32-react-routerv6/src/Root.tsx
+++ 32-react-routerv6/src/Root.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function Root() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
 
 export default Root;
--- 32-react-routerv6/src/Router.tsx
+++ 32-react-routerv6/src/Router.tsx
@@ -1,6 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from './Root';
+import Home from './screens/Home';
+import About from './screens/About';
 
-const router = createBrowserRouter([{ path: '/', element: <Root /> }]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/about', element: <About /> },
+    ],
+  },
+]);
 
 export default router;
