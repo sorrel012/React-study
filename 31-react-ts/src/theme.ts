38f3840197d251b7b40374f5f0302a306485aa48
sorrel012 <wain1719@gmail.com>
2024-03-05 21:54:34 +0900

    feat: Set styled theme files

--- 31-react-ts/src/theme.ts
+++ 31-react-ts/src/theme.ts
@@ -1,12 +1,12 @@
 import { DefaultTheme } from 'styled-components';
 
-const lightTheme: DefaultTheme = {
+export const lightTheme: DefaultTheme = {
   bgColor: 'white',
   textColor: 'black',
   btnColor: 'tomato',
 };
 
-const darkTheme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   bgColor: 'black',
   textColor: 'white',
   btnColor: 'teal',
