7e48c07a5cd213d17af2392456b168433b0fdf6b
sorrel012 <wain1719@gmail.com>
2024-03-05 21:43:47 +0900

    feat: Add types to form event

--- 31-react-ts/src/App.tsx
+++ 31-react-ts/src/App.tsx
@@ -1,10 +1,30 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="I'm here" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
