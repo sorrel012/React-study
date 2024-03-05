8b51847ff5b7524bc4d2cdf47310c0f0ae56fba0
sorrel012 <wain1719@gmail.com>
2024-03-05 21:53:48 +0900

    feat: Set styled theme files

---
+++ 31-react-ts/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+const lightTheme: DefaultTheme = {
+  bgColor: 'white',
+  textColor: 'black',
+  btnColor: 'tomato',
+};
+
+const darkTheme: DefaultTheme = {
+  bgColor: 'black',
+  textColor: 'white',
+  btnColor: 'teal',
+};
