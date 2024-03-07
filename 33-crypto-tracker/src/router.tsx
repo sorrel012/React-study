b006717dc7e4454fffadb8c08c2ca98e182c8ac4
sorrel012 <wain1719@gmail.com>
2024-03-07 13:14:59 +0900

    feat: Set router

---
+++ 33-crypto-tracker/src/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Coins from './routes/Coins';
+import Coin from './routes/Coin';
+
+const router = createBrowserRouter([
+  {path: '/', element: <Coins />},
+  {path: '/:coinId', element: <Coin /> }
+]);
+
+export default router;
---
+++ 33-crypto-tracker/src/routes/Coins.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Coins() {
+  return <div></div>;
+}
+
+export default Coins;
