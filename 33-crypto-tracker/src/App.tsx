fb1f66adddbbd7a0b8cd7a49c5d7e609eb95d883
sorrel012 <wain1719@gmail.com>
2024-03-07 15:56:08 +0900

    feat: Display coin list

--- 33-crypto-tracker/src/App.tsx
+++ 33-crypto-tracker/src/App.tsx
@@ -68,6 +68,7 @@ const GlobalStyle = createGlobalStyle`
   }
   a{
     text-decoration: none;
+    color: inherit;
   }
 `;
 
--- 33-crypto-tracker/src/routes/Coins.tsx
+++ 33-crypto-tracker/src/routes/Coins.tsx
@@ -1,12 +1,79 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  padding: 20px 20px 0;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px;
+  border-radius: 15px;
+  margin-bottom: 10px;
+`;
 
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+const coins = [
+  {
+    id: 'btc-bitcoin',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'eth-ethereum',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'hex-hex',
+    name: 'HEX',
+    symbol: 'HEX',
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: 'token',
+  },
+];
+
 function Coins() {
-  return <Title>코인</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={'/' + coin.id}>{coin.name}</Link>
+          </Coin>
+        ))}
+      </CoinList>
+    </Container>
+  );
 }
 
 export default Coins;
