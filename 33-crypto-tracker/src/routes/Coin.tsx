f371d07e458eb761e770361144cc410866aaf8e0
sorrel012 <wain1719@gmail.com>
2024-03-09 21:37:45 +0900

    feat: Add style to styled-component

--- 33-crypto-tracker/src/routes/Coin.tsx
+++ 33-crypto-tracker/src/routes/Coin.tsx
@@ -26,6 +26,28 @@ const Loader = styled.span`
   display: block;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0;
+`;
+
 interface LocationInterface {
   state: {
     name: string;
