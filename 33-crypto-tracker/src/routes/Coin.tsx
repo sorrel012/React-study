136ee6e0599f57805f014e8136228af4a032a1d2
sorrel012 <wain1719@gmail.com>
2024-03-09 22:28:28 +0900

    feat: Add styling to Tab & Tabs using styled-components

--- 33-crypto-tracker/src/routes/Coin.tsx
+++ 33-crypto-tracker/src/routes/Coin.tsx
@@ -33,6 +33,7 @@ const Overview = styled.div`
   padding: 10px 20px;
   border-radius: 10px;
 `;
+
 const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
@@ -44,6 +45,7 @@ const OverviewItem = styled.div`
     margin-bottom: 5px;
   }
 `;
+
 const Description = styled.p`
   margin: 20px 0;
 `;
@@ -54,6 +56,28 @@ interface LocationInterface {
     rank: number;
   };
 }
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
 
 interface IInfoData {
   id: string;
