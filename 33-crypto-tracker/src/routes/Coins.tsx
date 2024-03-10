9619b5c910af032f355721a94de9cdb859d7111f
sorrel012 <wain1719@gmail.com>
2024-03-10 22:12:40 +0900

    feat: Add styling to card background

--- 33-crypto-tracker/src/routes/Coins.tsx
+++ 33-crypto-tracker/src/routes/Coins.tsx
@@ -22,7 +22,7 @@ const Header = styled.header`
 const CoinList = styled.ul``;
 
 const Coin = styled.li`
-  background-color: white;
+  background-color: ${(props) => props.theme.cardBgColor};
   color: ${(props) => props.theme.textColor};
   border-radius: 15px;
   margin-bottom: 10px;
