4ddd746198c8af9f1944ae980f52ffc0df2b8b1a
sorrel012 <wain1719@gmail.com>
2024-03-12 17:25:58 +0900

    feat: Use register function in the useForm

--- 34-todo-app/src/ToDoList.tsx
+++ 34-todo-app/src/ToDoList.tsx
@@ -1,30 +1,49 @@
-import { useState } from 'react';
+// function ToDoList() {
+//   const [toDo, setToDo] = useState('');
+//   const [toDoError, setToDoError] = useState('');
+//
+//   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//
+//     setToDoError('');
+//     setToDo(value);
+//   };
+//
+//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (toDo.length < 10) {
+//       return setToDoError('To do should be longer');
+//     }
+//     console.log('submit');
+//   };
+//
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           onChange={handleChange}
+//           type="text"
+//           value={toDo}
+//           placeholder="Write a to do"
+//         />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+import { useForm } from 'react-hook-form';
 
 function ToDoList() {
-  const [toDo, setToDo] = useState('');
-
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-
-    setToDo(value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleChange}
-          type="text"
-          value={toDo}
-          placeholder="Write a to do"
-        />
+      <form>
+        <input {...register('toDo')} placeholder="Write a to do" />
         <button>Add</button>
       </form>
     </div>
