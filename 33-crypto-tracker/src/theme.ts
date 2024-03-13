0b175e703210c4e63f6b4a2ab9f8877f615b916b
sorrel012 <wain1719@gmail.com>
2024-03-13 21:45:09 +0900

    feat: Change theme color

--- 33-crypto-tracker/src/theme.ts
+++ 33-crypto-tracker/src/theme.ts
@@ -3,13 +3,13 @@ import { DefaultTheme } from 'styled-components';
 export const darkTheme: DefaultTheme = {
   bgColor: '#2f3640',
   textColor: 'white',
-  accentColor: '#9c88ff',
-  cardBgColor: 'transparent',
+  accentColor: '#CC4A60',
+  cardBgColor: 'rgba(0,0,0,0.5)',
 };
 
 export const lightTheme: DefaultTheme = {
-  bgColor: 'whitesmoke',
+  bgColor: '#F8EDEB',
   textColor: 'black',
-  accentColor: '#9c88ff',
+  accentColor: '#CC4A60',
   cardBgColor: 'white',
 };
