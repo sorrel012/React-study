2ea3c7558cfcd89331d7b39c39373521c665b9bd
sorrel012 <wain1719@gmail.com>
2024-03-14 17:59:07 +0900

    feat: Add styling to global

--- 35-trello-clone/src/App.tsx
+++ 35-trello-clone/src/App.tsx
@@ -1,37 +1,8 @@
 import React from 'react';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { GlobalStyle } from './index';
 
 function App() {
-  const handleDragEnd = () => {};
-
-  return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div>
-        <Droppable droppableId="one">
-          {(provided) => (
-            <ul ref={provided.innerRef} {...provided.droppableProps}>
-              <Draggable draggableId="first" index={0}>
-                {(provided) => (
-                  <li ref={provided.innerRef} {...provided.draggableProps}>
-                    <span {...provided.dragHandleProps}>🔥</span>
-                    One
-                  </li>
-                )}
-              </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {(provided) => (
-                  <li ref={provided.innerRef} {...provided.draggableProps}>
-                    <span {...provided.dragHandleProps}>🔥</span>
-                    Two
-                  </li>
-                )}
-              </Draggable>
-            </ul>
-          )}
-        </Droppable>
-      </div>
-    </DragDropContext>
-  );
+  return <GlobalStyle />;
 }
 
 export default App;
